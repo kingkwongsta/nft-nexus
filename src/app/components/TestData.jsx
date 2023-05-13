9efb60b80c86875ee4968d9b3b71616000bbe692
Bryant Kwong <bryant.j.kwong@gmail.com>
2023-05-12 23:17:44 -0700

    boilerplate component fetch code

--- src/app/components/TestData.jsx
+++ src/app/components/TestData.jsx
@@ -1,9 +1,38 @@
 "use client";
+import React, { useState, useEffect } from "react";
 
-export default function TestData() {
+const TestData = () => {
+  const [nftData, setNftData] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("api/web3/nft-top-eth");
+        const jsonData = await response.json();
+        setData(jsonData);
+        setLoading(false);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div>
-      <h2>Test Data Component</h2>
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <ul>
+          {nftData.map((item) => (
+            <li key={item._id}>{item.nfts.contract.name}</li>
+          ))}
+        </ul>
+      )}
     </div>
   );
-}
+};
+
+export default TestData;
