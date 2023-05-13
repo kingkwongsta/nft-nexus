543093c9c6336b75ffda33c476b54a09d433a59d
Bryant Kwong <bryant.j.kwong@gmail.com>
2023-05-12 23:09:33 -0700

    testdata component works - connected to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
+import TestData from "./components/testData";
 
 export default function Home() {
   return (
     <main>
       <h2>Hello World</h2>
+      <TestData />
     </main>
   );
 }
