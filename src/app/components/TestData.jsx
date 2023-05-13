71f526c26b34023b67eefa35fd751e1773c4f535
Bryant Kwong <bryant.j.kwong@gmail.com>
2023-05-13 00:03:26 -0700

    render the name of each data item mapped

--- src/app/components/TestData.jsx
+++ src/app/components/TestData.jsx
@@ -2,37 +2,36 @@
 import React, { useState, useEffect } from "react";
 
 const TestData = () => {
-  const [nftData, setNftData] = useState(null);
+  const [nftData, setNftData] = useState();
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch("api/web3/nft-top-eth");
-        const jsonData = await response.json();
-        setData(jsonData);
-        setLoading(false);
-      } catch (error) {
-        console.error(error);
-      }
-    };
-
     fetchData();
   }, []);
 
-  return (
-    <div>
-      {loading ? (
-        <p>Loading...</p>
-      ) : (
-        <ul>
-          {nftData.map((item) => (
-            <li key={item._id}>{item.nfts.contract.name}</li>
-          ))}
-        </ul>
-      )}
-    </div>
-  );
+  const fetchData = async () => {
+    try {
+      const response = await fetch("api/web3/nft-top-eth");
+      const jsonData = await response.json();
+      console.log(jsonData);
+      setNftData(jsonData);
+      setLoading(false);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const renderNftCard = () => {
+    nftData.map((item) => {
+      <NftCard key={item._id} nft={item.contract} />;
+    });
+  };
+
+  return <div>{loading ? <p>Loading...</p> : renderNftCard()}</div>;
 };
 
 export default TestData;
+
+const NftCard = ({ nft }) => {
+  return <div>{nft.name}</div>;
+};
