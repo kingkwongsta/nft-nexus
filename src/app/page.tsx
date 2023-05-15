0cec6f71040a8ca11b4425d1e0b19d6e24a173cb
Bryant Kwong <bryant.j.kwong@gmail.com>
2023-05-14 22:35:24 -0700

    add into homepage and remove use client

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 import Image from "next/image";
 import TestData from "./components/TestData";
+import TestDataSSR from "./components/TestDataSSR";
 
 export default function Home() {
   return (
     <main>
       <h2>Hello</h2>
-      <TestData />
+      <TestDataSSR />
     </main>
   );
 }
