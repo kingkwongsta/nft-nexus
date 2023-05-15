1d45026b20fe845ede04935b9518a661207afbe5
Bryant Kwong <bryant.j.kwong@gmail.com>
2023-05-14 22:14:37 -0700

    add image whitelist and image grid

--- src/app/components/TestData.jsx
+++ src/app/components/TestData.jsx
@@ -1,23 +1,33 @@
 "use client";
 import React, { useState, useEffect } from "react";
+import Image from "next/image";
 
 const NftCard = ({ nft }) => {
   console.log(nft);
-  return <div>{nft.name}</div>;
+  return (
+    <div>
+      <div>
+        <Image
+          src={nft.metadata.cached_thumbnail_url}
+          width={200}
+          height={200}
+          alt="collection image"
+        />
+      </div>
+      <h2>{nft.name}</h2>
+    </div>
+  );
 };
 
 const TestData = () => {
-  console.log("run");
   const [nftData, setNftData] = useState();
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    console.log("useEffect");
     fetchData();
   }, []);
 
   const fetchData = async () => {
-    console.log("fetch data is running");
     try {
       const response = await fetch("api/web3/nft-top-eth");
       const jsonData = await response.json();
@@ -30,13 +40,16 @@ const TestData = () => {
   };
 
   const renderNftCard = () => {
-    console.log("state date is: ", nftData);
     return nftData.map((item) => {
       return <NftCard key={item._id} nft={item.contract} />;
     });
   };
 
-  return <div>{loading ? <p>LOADING...</p> : renderNftCard()}</div>;
+  return (
+    <div className="grid grid-cols-4 gap-5">
+      {loading ? <p>LOADING...</p> : renderNftCard()}
+    </div>
+  );
 };
 
 export default TestData;
