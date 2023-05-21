d341ccad290699fa0821209f94a3318de2d0004c
kwongster <bryant.j.kwong@gmail.com>
2023-05-20 21:11:45 -0700

    setup data fetching in collection page

--- src/pages/collection/[slug].js
+++ src/pages/collection/[slug].js
@@ -1,8 +1,23 @@
+"use client";
 import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 
-export default function Page({ collection }) {
+export default function Page() {
+  const [collectionData, setCollectionData] = useState();
+
   const router = useRouter();
-  console.log(collection);
+
+  const fetchData = async () => {
+    try {
+      const res = await fetch("api/web3/collection");
+      const data = await res.json();
+      setCollectionData(data);
+      console.log(data);
+    } catch (error) {
+      return error;
+    }
+  };
+
   return (
     <div>
       <p>Collection: {router.query.slug}</p>
