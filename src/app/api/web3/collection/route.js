12100c4bf6617b80c3f21a0621f15c19907cadac
kwongster <bryant.j.kwong@gmail.com>
2023-05-20 21:08:09 -0700

    remove props in link component

---
+++ src/app/api/web3/collection/route.js
@@ -0,0 +1,11 @@
+import { getData } from "../../../../../lib/mongo/web3/collection";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  try {
+    const { nfts } = await getData();
+    return NextResponse.json(nfts);
+  } catch (error) {
+    return NextResponse.json({ error });
+  }
+}
