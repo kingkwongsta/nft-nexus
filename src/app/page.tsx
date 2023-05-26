451efb2be23cd6ef9cf75fd52f28aa3f8a91f013
Bryant Kwong <bryant.j.kwong@gmail.com>
2023-05-26 11:47:09 -0700

    setup fetch data to store

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
+"use client";
+import { useEffect } from "react";
 import Popular from "./components/popular/Popular";
 import Test from "./components/Test";
 import { useSelector, useDispatch } from "react-redux";
+import { fetchInitialData } from "@/redux/features/top-nft-eth/topNftEthSlice";
 
 export default function Home() {
   const topNftEth = useSelector((state) => state.topNftEth);
