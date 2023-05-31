d07b2e4d5c4f332963791349cf07afe9ae2784ce
Bryant Kwong <bryant.j.kwong@gmail.com>
2023-05-30 20:34:28 -0700

    move provider and set useSelector for collection pages

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <Providers>
+        <div className="m-5 w-max text-center">
+          <h2>HEADER HERE</h2>
+        </div>
         <body className={inter.className}>{children}</body>
       </Providers>
     </html>
--- src/pages/collection/[slug].js
+++ src/pages/collection/[slug].js
@@ -1,77 +1,43 @@
-// "use client";
-// import { useRouter } from "next/router";
-// import Image from "next/image";
-// import { useEffect, useState } from "react";
-
-// export default function Page() {
-//   const [collectionData, setCollectionData] = useState();
-//   const router = useRouter();
-
-//   useEffect(() => {
-//     fetchData();
-//   }, []);
-
-//   const fetchData = async () => {
-//     const options = {
-//       headers: {
-//         collection: router.query.slug,
-//       },
-//     };
-//     try {
-//       const res = await fetch("/api/web3/collection", options);
-//       const data = await res.json();
-//       setCollectionData(data);
-//       console.log(data);
-//     } catch (error) {
-//       return error;
-//     }
-//   };
-
-//   function renderCollectionGallery() {
-//     return collectionData.nfts.map((nft, index) => {
-//       return <Gallery key={index} nft={nft} />;
-//     });
-//   }
-
-//   return (
-//     <div>
-//       <p>Collection: {router.query.slug}</p>
-//       {collectionData ? renderCollectionGallery() : <p>no data</p>}
-//     </div>
-//   );
-// }
-
-// function Gallery({ nft }) {
-//   return (
-//     <div className="card">
-//       {nft.cached_file_url && (
-//         <Image
-//           className=""
-//           src={nft.cached_file_url}
-//           width="250"
-//           height="250"
-//           alt="bayc"
-//         />
-//       )}
-//       <p className="text-lg my-2 mb-8">#{nft.token_id}</p>
-//     </div>
-//   );
-// }
-
+"use client";
 import { useRouter } from "next/router";
-import { useSearchParams } from "next/navigation";
+import Image from "next/image";
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 
-const Collection = () => {
+export default function Page() {
+  const [collectionData, setCollectionData] = useState();
   const router = useRouter();
-  const data = router.query;
-  const searchParams = useSearchParams();
-  console.log(searchParams.get("search"));
+  // const topNftEth = useSelector((state) => state.topNftEth);
+
+  function renderCollectionGallery() {
+    return collectionData.nfts.map((nft, index) => {
+      return <Gallery key={index} nft={nft} />;
+    });
+  }
 
   return (
     <div>
-      <h1>Collection: </h1>
+      {/* <p>Collection: {router.query.slug}</p> */}
+      {/* {collectionData ? renderCollectionGallery() : <p>no data</p>} */}
+      {/* <p>{topNftEth[0].contract.name}</p> */}
+      <p>hello worldz</p>
     </div>
   );
-};
+}
 
-export default Collection;
+function Gallery() {
+  return (
+    <div className="card">
+      {nft.cached_file_url && (
+        <Image
+          className=""
+          src={nft.cached_file_url}
+          width="250"
+          height="250"
+          alt="bayc"
+        />
+      )}
+      <p className="text-lg my-2 mb-8">#{nft.token_id}</p>
+    </div>
+  );
+}
