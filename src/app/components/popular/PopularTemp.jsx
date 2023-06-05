a74b1bda83bb077e09d5030cc617d54710161935
kwongster <bryant.j.kwong@gmail.com>
2023-06-04 23:19:15 -0700

    remove code and replace with placeholder dta

--- src/app/components/popular/PopularTemp.jsx
+++ src/app/components/popular/PopularTemp.jsx
@@ -1,19 +1,19 @@
 "use client";
-import PopularCard from "./PopularCard";
 
 const TempCards = () => {
   return (
     <div className="flex flex-col items-center max-w-[250px] rounded-lg border-[1px] drop-shadow-lg px-3 py-1">
       <div className="min-w-[200px] min-h-[200px]">
-        <Image
+        {/* <Image
           className="object-cover h-[200px] mb-4 mt-2 rounded-md"
           src={collection.metadata.cached_thumbnail_url}
           width={200}
           height={200}
           alt={collection.name}
-        />
+        /> */}
+        HELLO
       </div>
-      <h2 className="text-lg font-medium mb-2">{collection.name}</h2>
+      <h2 className="text-lg font-medium mb-2">NAME</h2>
     </div>
   );
 };
@@ -26,7 +26,7 @@ const Popular = () => {
       <div>
         <div className="grid grid-cols-3 gap-10">
           {renderNftCard.map((x, index) => {
-            return <PopularCard key={index} />;
+            return <TempCards key={index} />;
           })}
         </div>
       </div>
