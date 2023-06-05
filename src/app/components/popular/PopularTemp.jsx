f3c79c096c81f0dc8e15bf9c12649608c0eb309a
kwongster <bryant.j.kwong@gmail.com>
2023-06-04 23:18:16 -0700

    remove more code

--- src/app/components/popular/PopularTemp.jsx
+++ src/app/components/popular/PopularTemp.jsx
@@ -1,40 +1,22 @@
 "use client";
 import PopularCard from "./PopularCard";
 
-const TempCards = () =>{
-    return(      
-        const PopularCard = ({ collection, nfts }) => {
-          return (
-            <div className="flex flex-col items-center max-w-[250px] rounded-lg border-[1px] drop-shadow-lg px-3 py-1">
-              {/* <Link href={`/collection/${collection.name}`}> */}
-              <Link
-                href={{
-                  pathname: "{`/collection/${collection.name}`}",
-                  query: nfts,
-                }}
-                as={`/collection/${collection.name}`}
-              >
-                <div className="min-w-[200px] min-h-[200px]">
-                  <Image
-                    className="object-cover h-[200px] mb-4 mt-2 rounded-md"
-                    src={collection.metadata.cached_thumbnail_url}
-                    width={200}
-                    height={200}
-                    alt={collection.name}
-                  />
-                </div>
-                <h2 className="text-lg font-medium mb-2">{collection.name}</h2>
-              </Link>
-            </div>
-          );
-        };
-        
-        export default PopularCard;
-        
-
-    )
-}
-
+const TempCards = () => {
+  return (
+    <div className="flex flex-col items-center max-w-[250px] rounded-lg border-[1px] drop-shadow-lg px-3 py-1">
+      <div className="min-w-[200px] min-h-[200px]">
+        <Image
+          className="object-cover h-[200px] mb-4 mt-2 rounded-md"
+          src={collection.metadata.cached_thumbnail_url}
+          width={200}
+          height={200}
+          alt={collection.name}
+        />
+      </div>
+      <h2 className="text-lg font-medium mb-2">{collection.name}</h2>
+    </div>
+  );
+};
 
 //main component to export
 const Popular = () => {
@@ -55,5 +37,3 @@ const Popular = () => {
 };
 
 export default Popular;
-
-
