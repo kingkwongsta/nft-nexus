0010e47c39c353190e24bc7619fa8981ab9dfb23
Bryant Kwong <bryant.j.kwong@gmail.com>
2023-06-05 15:24:23 -0700

    make bgd color even darker

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
   }, []);
 
   return (
-    <main className="bg-[#3B3B3B] flex flex-col items-center justify-center">
+    <main className="bg-[#2B2B2B] flex flex-col items-center justify-center">
       <Test />
       {/* <PopularTemp /> */}
       <Popular />
