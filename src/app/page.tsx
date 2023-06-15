3443fd38b75a03af4941b91a5c20c4063b3d02ca
Bryant Kwong <bryant.j.kwong@gmail.com>
2023-06-14 21:31:42 -0700

    remove test code from main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,7 @@ import { useEffect } from "react";
 import Popular from "./components/popular/Popular";
 import { useSelector, useDispatch } from "react-redux";
 import { fetchInitialData } from "@/redux/features/top-nft-eth/topNftEthSlice";
-import Test from "./components/Test";
 import Hero from "./components/hero/hero";
-import PopularTemp from "./components/popular/PopularTemp";
 import { RootState } from "../shared/types";
 import { useAppDispatch } from "@/shared/hooks";
 
@@ -19,7 +17,6 @@ export default function Home() {
 
   return (
     <main className="bg-[#2B2B2B] flex flex-col items-center justify-center">
-      {/* <Test /> */}
       <Hero />
       <Popular />
     </main>
