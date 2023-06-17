80fdc26ce8bcdfcefbee76ddd361c7777d419813
kwongster <bryant.j.kwong@gmail.com>
2023-06-16 22:42:53 -0700

    remove comments

--- src/pages/collection/[slug].js
+++ src/pages/collection/[slug].js
@@ -1,44 +1,44 @@
-// "use client";
-// import { useRouter } from "next/router";
-// import Image from "next/image";
-// import { useSelector } from "react-redux";
+"use client";
+import { useRouter } from "next/router";
+import Image from "next/image";
+import { useSelector } from "react-redux";
 
-// export default function Page() {
-//   const router = useRouter();
-//   const topNftEth = useSelector((state) => state.topNftEth);
+export default function Page() {
+  const router = useRouter();
+  const topNftEth = useSelector((state) => state.topNftEth);
 
-//   function renderCollectionGallery() {
-//     return collectionData.nfts.map((nft, index) => {
-//       return <Gallery key={index} nft={nft} />;
-//     });
-//   }
-//   function getState() {
-//     console.log(topNftEth);
-//   }
+  function renderCollectionGallery() {
+    return collectionData.nfts.map((nft, index) => {
+      return <Gallery key={index} nft={nft} />;
+    });
+  }
+  function getState() {
+    console.log(topNftEth);
+  }
 
-//   return (
-//     <div>
-//       <p>hello worldz</p>
-//       <button onClick={getState} className="m-5">
-//         click me
-//       </button>
-//     </div>
-//   );
-// }
+  return (
+    <div>
+      <p>hello worldz</p>
+      <button onClick={getState} className="m-5">
+        click me
+      </button>
+    </div>
+  );
+}
 
-// function Gallery() {
-//   return (
-//     <div className="card">
-//       {nft.cached_file_url && (
-//         <Image
-//           className=""
-//           src={nft.cached_file_url}
-//           width="250"
-//           height="250"
-//           alt="bayc"
-//         />
-//       )}
-//       <p className="text-lg my-2 mb-8">#{nft.token_id}</p>
-//     </div>
-//   );
-// }
+function Gallery() {
+  return (
+    <div className="card">
+      {nft.cached_file_url && (
+        <Image
+          className=""
+          src={nft.cached_file_url}
+          width="250"
+          height="250"
+          alt="bayc"
+        />
+      )}
+      <p className="text-lg my-2 mb-8">#{nft.token_id}</p>
+    </div>
+  );
+}
