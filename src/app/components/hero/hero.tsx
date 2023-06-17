334f5a1cc9d05ce12bc989b180ef5ea032ea3d67
kwongster <bryant.j.kwong@gmail.com>
2023-06-16 22:48:48 -0700

    create smaller border image for nft

--- src/app/components/hero/hero.tsx
+++ src/app/components/hero/hero.tsx
@@ -77,8 +77,10 @@ export default function Hero() {
         </button>
       </div>
       {/* RIGHT CONTENT */}
-      <div className="hero-right sm:max-lg:invisible sm:max-lg:h-0 sm:mt-10 lg:mt-0 flex-auto basis-1/2 justify-center h-[400px] sm:max-lg:max-h-[300px] bg-zinc-700">
-        <div className="hero-img rounded-lg pt-10 px-16 w-[500px] ">
+      <div className="hero-right flex-auto basis-1/2 justify-center">
+        <div className="max-w-[400px] bg-zinc-700 rounded-xl">
+          {" "}
+          <div className="hero-img rounded-lg pt-10 px-16 w-[500px] "></div>
           <Link href={`/collection/`}>
             {storeLoading ? (
               <Image
