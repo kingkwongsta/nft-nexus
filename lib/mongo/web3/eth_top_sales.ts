fb7379d339c7ba282cdc419f3d9c9355f9a6cd9c
Bryant Kwong <bryant.j.kwong@gmail.com>
2023-06-26 19:28:34 -0700

    setup monogodb connection for sales data

---
+++ lib/mongo/web3/eth_top_sales.ts
@@ -0,0 +1,30 @@
+import clientPromise from "../index";
+
+let client: any, db: any, nfts: any;
+
+async function init(): Promise<void> {
+  if (db) return;
+  try {
+    client = await clientPromise;
+    db = await client.db("web3");
+    nfts = await db.collection("eth_top_sales");
+    console.log("successfully connected to web3/eth_top_sales");
+  } catch (error) {
+    throw new Error(`No connection with database established - ${error}`);
+  }
+}
+
+(async (): Promise<void> => {
+  await init();
+})();
+
+export async function getData(): Promise<{ nfts?: any[]; error?: string }> {
+  try {
+    if (!nfts) await init();
+    const result = await nfts.find({}).toArray();
+    console.log("successfully getData from web3/eth_top_sales");
+    return { nfts: result };
+  } catch (error) {
+    return { error: "Failed to fetch transactions!" };
+  }
+}
