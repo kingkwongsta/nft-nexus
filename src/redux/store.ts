93bb85b54d2b5c10915b5e5d74f5356300c250f7
Bryant Kwong <bryant.j.kwong@gmail.com>
2023-06-26 19:53:51 -0700

    add sales data to store

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -2,10 +2,12 @@ import { configureStore } from "@reduxjs/toolkit";
 // import counterReducer from "./features/counter/counterSlice";
 // import pokemonReducer from "./features/pokemon/pokemonSlice";
 import topNftEthReducer from "./features/top-nft-eth/topNftEthSlice";
+import ethSalesReducer from "./features/eth-top-sales/ethTopSalesSlice";
 
 export const store = configureStore({
   reducer: {
     topNftEth: topNftEthReducer,
+    ethSales: ethSalesReducer,
   },
 });
 
