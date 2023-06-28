8f17e3e3ccd51acd620c93d248fd1fd09ebe39a1
Bryant Kwong <bryant.j.kwong@gmail.com>
2023-06-27 18:39:22 -0700

    move sales data to get prop function

--- src/shared/types.ts
+++ src/shared/types.ts
@@ -33,6 +33,46 @@ export interface salesType {
   };
 }
 
+export interface salesData {
+  chain: string;
+  contract_address: string;
+  token_id: string;
+  metadata_url: string;
+  metadata: {
+    image: string;
+    name: string;
+    attributes: {
+      trait_type: string;
+      value: string;
+    }[];
+  };
+  file_information: null | string;
+  file_url: string;
+  animation_url: null | string;
+  cached_file_url: string | null;
+  cached_animation_url: null | string;
+  creator_address: string;
+  mint_date: string;
+  updated_date: string;
+  owner: string;
+  rarity: {
+    strategy: string;
+    score: number;
+    rank: number;
+    collection_size: number;
+    updated_date: string;
+  };
+  attributes: {
+    trait_type: string;
+    display_type: null | string;
+    value: string;
+    statistics: {
+      total_count: number;
+      prevalence: number;
+    };
+  }[];
+}
+
 export interface RootState {
   ethSales: any;
   topNftEth: TopNftEthState;
